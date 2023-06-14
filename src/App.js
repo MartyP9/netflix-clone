14894d9e856abdeccd43459569643b14a2812b3c
Peter Martens <superiorbrews@gmail.com>
2023-06-13 23:32:13 -0500

    did some work on banner and got api working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import { Counter } from './features/counter/Counter';
 import './assets/css/App.css';
 import HomeScreen from './HomeScreen';
+// const dotenv = require('dotenv')
+// require('dotenv').config({path: './config/.env'})
 
 function App() {
   return (
