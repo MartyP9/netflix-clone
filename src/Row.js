71155bd116f2e3eaeb8af4bf831b7ecbf2807a48
Peter Martens <superiorbrews@gmail.com>
2023-06-15 09:19:23 -0500

    Fixed caption display on images

--- src/Row.js
+++ src/Row.js
@@ -23,16 +23,18 @@ const Row = ({title, fetchUrl, isLargeRow=false}) => {
                 {movies.map((movie)=>
                     ((isLargeRow && movie.poster_path) ||
                     (!isLargeRow && movie.backdrop_path)) && (
-                        <><img key={movie.id}
-                        src={`${base_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path
-                        }`} alt={movie.name}
-                        className={`row-poster ${isLargeRow && 'row-posterLarge'}`}
-                        title={movie.title || movie.name || movie.original_name}
-                        />
+                        <>
+                            <figure className='card'>
+                            <img key={movie.id}
+                            src={`${base_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                            }`} alt={movie.name}
+                            className={`row-poster ${isLargeRow && 'row-posterLarge'}`}                            />
+                            <figcaption>{movie.title || movie.name || movie.original_name}</figcaption>
+                            </figure>    
                         </>
                     )
                 )}
-            </div>
+        </div>
     </div>
   )
 }
